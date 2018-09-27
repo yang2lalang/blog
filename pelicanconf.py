084810b157646b42ca0a821c6711efa0b6113ba6
franklin <franklinokoli2000@gmail.com>
2018-09-27 17:03:39 +0200

    First Draft of the pelican site

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Franklin OKOLI'
 SITENAME = u'Franklin is Blogging'
-SITEURL = 'https://yang2lalang.github.io'
+SITEURL = 'http://localhost:8000'
 RELATIVE_URLS = True
 RELATIVE_SITEURL = '' if RELATIVE_URLS else SITEURL
 SITETITLE = AUTHOR
@@ -12,6 +12,7 @@ SITESUBTITLE = 'Robotics Software Engineer, Part time Trader,  Consultant'
 SITEDESCRIPTION = '%s\'s My thoughts and a few tutorials' % AUTHOR
 SITELOGO = './images/cartoon_ik.png'
 PATH = 'content'
+
 BROWSER_COLOR = '#333333'
 PYGMENTS_STYLE = 'monokai'
 
@@ -28,23 +29,15 @@ DATE_FORMATS = {
     'en': '%B %d, %Y',
 }
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
 USE_FOLDER_AS_CATEGORY = False
 MAIN_MENU = True
 HOME_HIDE_TAGS = True
 
-LINKS = (('Home', 'https://yang2lalang.github.io'),)
+LINKS = (('Home', 'https://yang2lalang.github.io'),('Researchgate', 'https://www.researchgate.net/profile/Franklin_Okoli'),)
 
 
-
-SOCIAL = (('Linkedin', 'https://www.linkedin.com/in/franklinokoli/'),
-	  ('Github', 'https://github.com/yang2lalang'),
-          ('Researchgate', 'https://www.researchgate.net/profile/Franklin_Okoli'))
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/franklinokoli/'),
+	  ('github', 'https://github.com/yang2lalang'),)
 
 MENUITEMS = (('Archives', '/archives.html'),
              ('Categories', '/categories.html'),
@@ -63,8 +56,8 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #
 
-MARKUP = ('md', 'ipynb')
-STATIC_PATHS = ['images']
+MARKUP = ('md')
+STATIC_PATHS = ['images', 'extra','articles','pages']
 PLUGIN_PATH = './pelican-plugins'
 PLUGINS = ['sitemap', 'post_stats','i18n_subsites']
 IGNORE_FILES = ['.ipynb_checkpoints']
@@ -91,15 +84,8 @@ SITEMAP = {
 DISQUS_SITENAME = "http-yang2lalang-github-io"
 ADD_THIS_ID = 'ra-5bac97e477d3d598'
 
-STATIC_PATHS = ['images', 'extra']
 
-EXTRA_PATH_METADATA = {
-    'extra/custom.css': {'path': 'static/custom.css'},
-}
 
-CUSTOM_CSS = 'static/custom.css'
-
-USE_LESS = True
 
 GOOGLE_ADSENSE = {
     'ca_id': 'ca-pub-9421131783612830',
